daa4343c7acbd23142bde39d58bc8edd31a5a48e
Naheed Anjum <97374879+NaheedAnju@users.noreply.github.com>
2022-02-12 23:54:19 +0530

    Add files via upload

---
+++ Assignment 3 Result NaheedAnjum.py
@@ -0,0 +1,84 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\HP\AppData\Local\Programs\Python\Python37-32\PEC Practical.py 
+-------------------------Question 1-------------------------
+Enter a single word - NaheedAnjum
+No. of a =  2
+No. of b =  0
+No. of c =  0
+No. of d =  1
+No. of e =  2
+No. of f =  0
+No. of g =  0
+No. of h =  1
+No. of i =  0
+No. of j =  0
+No. of k =  1
+No. of l =  0
+No. of m =  1
+No. of n =  2
+No. of o =  0
+No. of p =  0
+No. of q =  0
+No. of r =  0
+No. of s =  0
+No. of t =  0
+No. of u =  1
+No. of v =  0
+No. of w =  0
+No. of x =  0
+No. of y =  0
+No. of z =  0
+-------------------------Question 2-------------------------
+Enter  DAY - 28
+Enter MONTH - 02
+Enter YEAR - 1999
+Next date is :  1 / 3 / 1999
+-------------------------Question 3-------------------------
+Enter number of values to input in list- 3
+Enter Value- 3
+Enter Value- 9
+Enter Value- 10
+[(3, 9), (9, 81), (10, 100)]
+-------------------------Question 4-------------------------
+Enter grade points - 9
+Your Grade is 'A' and Excellent Performance
+-------------------------Question 5-------------------------
+Enter no. of rows - 6
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+-------------------------Question 6-------------------------
+Enter student's SID - 211
+Enter student' name - Naheed
+Do you want to add more name (Y/N)?Y
+Enter student's SID - 210
+Enter student' name - Anjum
+Do you want to add more name (Y/N)?Y
+Enter student's SID - 222
+Enter student' name - Naaz
+Do you want to add more name (Y/N)?Y
+Enter student's SID - 215
+Enter student' name - Atif
+Do you want to add more name (Y/N)?N
+Student Details -  {'211': 'Naheed', '210': 'Anjum', '222': 'Naaz', '215': 'Atif'}
+sorted dictiionary on the basis of name -  {'210': 'Anjum', '215': 'Atif', '222': 'Naaz', '211': 'Naheed'}
+sorted dictiionary on the basis of SID -  {'210': 'Anjum', '211': 'Naheed', '215': 'Atif', '222': 'Naaz'}
+Enter SID of student to search name - 222
+Student's name with sid 222 is Naaz
+-------------------------Question 7-------------------------
+Enter limit - 100
+0 1 1 2 3 5 8 13 21 34 55 89 
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+Average of Fiboncci Series -  19.333333333333332
+-------------------------Question 8-------------------------
+New set of all elements that are in Set1 and Set2 but not both {1, 3, 5, 6, 8}
+New set of all elements that are in only one of the three sets Set1, Set2 and Set3 {17, 3, 6, 8, 9, 13}
+New set of elements that are exactly two of the sets Set1, Set2 and Set3 {1, 2, 4, 5}
+New set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+New set of all integers in the range 1 to 10 that are not in Set1 , Set2 , Set3 {10, 7}
+>>> 
